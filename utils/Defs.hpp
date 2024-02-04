5f535d838c4cd1496ecb3440ba5a84a36c857eb6
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-02-04 16:24:45 +0100

    adding some typedefs

--- utils/Defs.hpp
+++ utils/Defs.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 14:36:15 by mel-yous          #+#    #+#             */
-/*   Updated: 2024/02/03 14:36:52 by mel-yous         ###   ########.fr       */
+/*   Updated: 2024/02/04 16:03:20 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 #include <vector>
 #include <utility>
+#include <map>
+
+class ServerContext;
+class Token;
 
 typedef std::pair<std::string, std::vector<std::string> > Directive;
+typedef std::map<std::string, std::vector<std::string> >  DirectivesMap;
+typedef std::vector<ServerContext> ServersVector;
+typedef std::vector<Token> TokensVector;
