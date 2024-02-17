6d32ebe395602cb645b151e8370137e06221d279
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-02-17 17:05:53 +0100

    defs

--- utils/Defs.hpp
+++ utils/Defs.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 14:36:15 by mel-yous          #+#    #+#             */
-/*   Updated: 2024/02/12 18:13:38 by mel-yous         ###   ########.fr       */
+/*   Updated: 2024/02/16 12:52:21 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <vector>
 #include <utility>
 #include <map>
+#include <iostream>
 
 class ServerContext;
 class Token;
