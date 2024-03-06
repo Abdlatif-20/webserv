33ed5c0fb4c3b456e06be1bc87fd383347f54531
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-03-06 10:33:37 +0100

    adding unknown_mimetype def

--- utils/Defs.hpp
+++ utils/Defs.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 14:36:15 by mel-yous          #+#    #+#             */
-/*   Updated: 2024/03/05 18:58:53 by mel-yous         ###   ########.fr       */
+/*   Updated: 2024/03/06 10:32:57 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ class LocationContext;
 
 #define HTTP_VERSION "HTTP/1.1"
 #define SERVER "WebServer 1.0"
+#define UNKNOWN_MIMETYPE "application/octet-stream"
 
 typedef std::vector<std::string> StringVector;
 typedef std::pair<std::string, StringVector> Directive;
@@ -30,7 +31,6 @@ typedef std::map<std::string, StringVector>  DirectivesMap;
 typedef std::vector<ServerContext> ServersVector;
 typedef std::vector<Token> TokensVector;
 typedef std::vector<LocationContext> LocationsVector;
-typedef std::pair<std::string, std::string> HostPort;
 typedef std::invalid_argument	Fail;
 
 typedef enum e_directive
