965d91af0b3646e63b4f5d16ff4cc515f517504f
hex01e <ouha531@gmail.com>
2024-03-13 13:13:06 +0000

    Exception when the file doesnt exist(reading fails returning -1) then using it as a length to append , fixed temporaly by seting the response done.

--- response/Response.cpp
+++ response/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: houmanso <houmanso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:07:22 by mel-yous          #+#    #+#             */
-/*   Updated: 2024/03/13 11:37:22 by houmanso         ###   ########.fr       */
+/*   Updated: 2024/03/13 11:49:40 by houmanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -157,12 +157,8 @@ void Response::prepareGETBody()
     }
     body.clear();
     ssize_t readedBytes = read(fd, buffer, sizeof(buffer));
-    if (readedBytes == -1)
-    {
-        /* Read fail */
-    }
-    if (readedBytes == 0)
-    {
+    if (readedBytes <= 0)
+	{
         responseDone = true;
         return;
     }
