eae36b6779add3fee6dc513b05e279860b2b896a
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-03-20 17:30:43 +0000

    update defs

--- utils/Defs.hpp
+++ utils/Defs.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Defs.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: houmanso <houmanso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 14:36:15 by mel-yous          #+#    #+#             */
-/*   Updated: 2024/03/12 17:08:08 by houmanso         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:49:01 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,22 @@ class LocationContext;
 #define SERVER "WebServer 1.0"
 #define UNKNOWN_MIMETYPE "application/octet-stream"
 #define SPACE " "
+
+#ifdef _WIN32
+#define OS_WINDOWS "Windows"
+
+#elif defined(__APPLE__)
+#define OS_MAC "macOS"
+
+#elif defined(__linux__)
+#define OS_LINUX "Linux"
+
+#elif defined(__unix__) || defined(__posix__)
+#define OS_UNIX "Unix"
+
+#else
+#define OS_UNKNOWN "Unknown"
+#endif
 
 typedef std::vector<std::string> StringVector;
 typedef std::pair<std::string, StringVector> Directive;
@@ -51,3 +67,19 @@ typedef enum e_directive
     UPLOAD_STORE,
     UNKNOWN
 } t_directive;
+
+enum StatusCodes
+{
+	OK = 200,
+	NotFound = 404,
+    FORBIDDEN = 403,
+	BadRequest = 400,
+	LengthRequired = 411,
+	NotImplemented = 501,
+	MovedPermanently = 301,
+	MethodNotAllowed = 405,
+	RequestURITooLong = 414,
+	RequestEntityTooLarge = 413,
+    InternalServerError = 500,
+	HTTPVersionNotSupported = 505
+};
